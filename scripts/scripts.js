90ab5dca987871c05e2b22c139338a8a89d30deb
pgeovany <pgeovany@dcc.ufrj.br>
2022-04-01 16:52:18 -0300

    checkout button added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,15 +4,22 @@ function select(element) {
     // const checkmark = element.querySelector("ion-icon");
     // checkmark.classList.toggle("hidden");
     const selected = element.parentElement.querySelector(".selected");
-    if(selected !== null) {
-        selected.classList.toggle("selected");
+    if(selected !== null && selected !== element) {
+        selected.classList.remove("selected");
         const checkmark = selected.querySelector("ion-icon");
         checkmark.classList.add("hidden");
     }
     else {
          element.classList.add("selected");
          const checkmark = element.querySelector("ion-icon");
-         checkmark.classList.toggle("hidden");
+         checkmark.classList.remove("hidden");
     }
-    console.log(selected);
+    // price = element.querySelector(".price");
+    // price = Number(price.innerHTML);
+    // console.log(price);
+    checkOut();
+}
+
+function checkout() {
+    alert("test");
 }
