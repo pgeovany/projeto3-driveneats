1692fed33fcda52742f01e26febfb0ecf5b5f06e
pgeovany <pgeovany@dcc.ufrj.br>
2022-04-02 00:17:58 -0300

    checkout button working properly. Still need to implement the checkout function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,29 +2,42 @@ function select(element) {
     const selected = element.parentElement.querySelector(".selected");
     const parent = element.parentElement;
 
-    if(selected !== null) {
+    if (selected !== null) {
         selected.classList.remove("selected");
         const checkmark = selected.querySelector("ion-icon");
         checkmark.classList.add("hidden");
         parent.classList.remove("check");
+        notReady();
     }
     else {
-         element.classList.add("selected");
-         const checkmark = element.querySelector("ion-icon");
-         checkmark.classList.remove("hidden");
-         parent.classList.add("check");
+        element.classList.add("selected");
+        const checkmark = element.querySelector("ion-icon");
+        checkmark.classList.remove("hidden");
+        parent.classList.add("check");
+        ready();
     }
-    isReady();
 }
 
-function isReady() {
+function ready() {
     mainCourse = document.querySelector(".mainCourse");
     drink = document.querySelector(".drink");
     dessert = document.querySelector(".dessert");
 
-    if (mainCourse.classList.contains("check") 
-    && drink.classList.contains("check") &&
-    dessert.classList.contains("check")) {
-        console.log("YES!")
+    if (mainCourse.classList.contains("check")
+        && drink.classList.contains("check")
+        && dessert.classList.contains("check")) {
+        
+        document.querySelector(".mainButton").classList.add("hidden");
+        document.querySelector(".checkoutButton").classList.remove("hidden");
+        
     }
+}
+
+function notReady() {
+    document.querySelector(".mainButton").classList.remove("hidden");
+    document.querySelector(".checkoutButton").classList.add("hidden");
+}
+
+function checkout() {
+    alert("Let's go!");
 }
